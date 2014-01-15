30e9358a37abb17909e1941c53b0a8f8a9b30dab
Kim Silkebækken <kim.silkebaekken@gmail.com>
2014-01-15 12:29:17 +0100

    Add presentations and politics resources

---
+++ app/assets/js/resources/presentations.js
@@ -0,0 +1,7 @@
+tumblr.resources.presentations = [
+	'i blog about {concepts.awesome}.',
+	'if you\'re a {insults.nouns} {insults.statements}.',
+	'fighting {concepts.terrible} / fighting {concepts.terrible} / fighting {concepts.terrible}',
+	'my pronuns are: {randomPronouns}.',
+	'my triggers include: {concepts.terrible}, {marginalized.nouns.concepts} and {marginalized.nouns.concepts}.',
+]
