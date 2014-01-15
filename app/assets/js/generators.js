36ad9d7d1949a8ea266748b97c6197c29657610f
Kim Silkebækken <kim.silkebaekken@gmail.com>
2014-01-15 12:41:12 +0100

    Add blog titles

--- app/assets/js/generators.js
+++ app/assets/js/generators.js
@@ -102,3 +102,44 @@ tumblr.generators.kins = function () {
 
 	return result
 }
+
+tumblr.generators.revolutions = function () {
+	var result = [],
+	    marginalized = [
+		    'PoC',
+		    'WoC',
+		    'aceo',
+		    'black',
+		    'chubby',
+		    'curvy',
+		    'deathfat',
+		    'demi',
+		    'diversity',
+		    'dysphoria',
+		    'ethnicity',
+		    'fandom',
+		    'fat',
+		    'fatty',
+		    'fem',
+		    'furry',
+		    'height',
+		    'homo',
+		    'latin',
+		    'lesb',
+		    'queer',
+		    'skinny',
+		    'trans',
+		    'womyn',
+		    'wymyn',
+	    ]
+
+	_.forEach(marginalized, function (group) {
+		if (group.slice(-1).search(/[aeiouy]/) === -1) {
+			// Word doesn't end in a wovel, add 'o'
+			group += 'o'
+		}
+		result.push((group + 'lution').toUpperCase())
+	})
+
+	return result
+}
---
+++ app/assets/js/resources/titles.js
@@ -0,0 +1,8 @@
+tumblr.resources.revolutions = tumblr.generators.revolutions()
+tumblr.resources.titles = [
+	'this is {privileged.nouns} privilege',
+	'the angry {marginalized.nouns}',
+	'{marginalized.nouns} microaggressions',
+	'this is social justice',
+	'{revolutions}',
+]
