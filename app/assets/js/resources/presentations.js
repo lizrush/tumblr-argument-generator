769aa8b981731db1866ec02696988f3f870f1c54
Kim Silkebækken <kim.silkebaekken@gmail.com>
2014-01-15 12:42:52 +0100

    Update presentations to use new resources

--- app/assets/js/resources/presentations.js
+++ app/assets/js/resources/presentations.js
@@ -2,6 +2,6 @@ tumblr.resources.presentations = [
 	'i blog about {concepts.awesome}.',
 	'if you\'re a {insults.nouns} {insults.statements}.',
 	'fighting {concepts.terrible} / fighting {concepts.terrible} / fighting {concepts.terrible}',
-	'my pronuns are: {randomPronouns}.',
-	'my triggers include: {concepts.terrible}, {marginalized.nouns.concepts} and {marginalized.nouns.concepts}.',
+	'my pronuns are: {joinedPronouns}.',
+	'my triggers include: {concepts.terrible}, {concepts.terrible} and {concepts.terrible}.',
 ]
